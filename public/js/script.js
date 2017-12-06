e9a2545df498f873542c41479fe67c42f2976910
student <tejaswil@gmail.com>
2017-12-06 12:43:52 -0700

    ac

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,64 @@
-var allNotes = "a49,a50,a51,a52,a53,a54,a55,a56,a57,a48,a81,a87,a69,a82,a84,a89,a85,a73,a79,a80,a65,a83,a68,a70,a71,a72,a74,a75,a76,a90,a88,a67,a86,a66,a78,a77,b49,b50,b52,b53,b54,b56,b57,b81,b87,b69,b84,b89,b73,b79,b80,b83,b68,b71,b72,b74,b76,b90,b67,b86,b66".split(",");
+var allNotes = ["a49",
+	"b49",
+	"a50",
+	"b50",
+	"a51",
+	"a52",
+	"b52",
+	"a53",
+	"b53",
+	"a54",
+	"b54",
+	"a55",
+	"a56",
+	"b56",
+	"a57",
+	"b57",
+	"a48",
+	"a81",
+	"b81",
+	"a87",
+	"b87",
+	"a69",
+	"b69",
+	"a82",
+	"a84",
+	"b84",
+	"a89",
+	"b89",
+	"a85",
+	"a73",
+	"b73",
+	"a79",
+	"b79",
+	"a80",
+	"b80",
+	"a65",
+	"a83",
+	"b83",
+	"a68",
+	"b68",
+	"a70",
+	"a71",
+	"b71",
+	"a72",
+	"b72",
+	"a74",
+	"b74",
+	"a75",
+	"a76",
+	"b76",
+	"a90",
+	"b90",
+	"a88",
+	"a67",
+	"b67",
+	"a86",
+	"b86",
+	"a66",
+	"b66",
+	"a78",
+	"a77"];
 
 function start()
 {
@@ -19,5 +79,6 @@ function setVolume(v)
 function playColor(note)
 {
   var n = allNotes.indexOf(note);
-  console.log(n);
+  console.log(note, n);
+  loadFile("./sendNote?n=" + n + "&v=100", function(data){})
 }
